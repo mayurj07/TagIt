1ea53f9f9e2666fd4bb33176d936cede839bbd38
Jain, Mayur Mayur.Jain@parc.com <Mayur.Jain@parc.com>
2015-12-02 17:05:03 -0800

    deleteNotebooks, notebooks completed

--- src/main/webapp/tagitapp/mvc/controllers/nodes.js
+++
@@ -1,4 +0,0 @@
-angular.module('app.controllers.nodes', []).
-  controller('NodesCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-
-  }]);
--- src/main/webapp/tagitapp/mvc/route.js
+++ src/main/webapp/tagitapp/mvc/route.js
@@ -9,19 +9,12 @@ angular.module('app', [
     'angularFileUpload',
 
     'app.services.ui',
-    'app.filters',
     'app.directives.nav',
-    'app.directives.fileupload',
     'app.controllers.home',
-    'app.controllers.nodes',
     'app.controllers.user',
-    'app.controllers.messenger',
-    'app.controllers.mail',
-    'app.controllers.report',
     'app.controllers.login',
     'app.controllers.notebook',
-    'app.controllers.bookmark',
-    'app.controllers.feedbacks'
+    'app.controllers.bookmark'
 ]).config(['$routeProvider', function ($routeProvider) {
 
         $routeProvider
@@ -52,58 +45,6 @@ angular.module('app', [
             .when('/bookmarks', {
                 templateUrl: 'mvc/views/bookmark.html',
                 controller: 'BookmarkCtrl'
-            })
-            .when('/user/users', {
-                templateUrl: 'mvc/views/user-users.html',
-                controller: 'UserUsersCtrl'
-            })
-            .when('/user/unconfirmed-users', {
-                templateUrl: 'mvc/views/user-unconfirmed.html',
-                controller: 'UserUnconfirmedUsersCtrl'
-            })
-            .when('/user/groups', {
-                templateUrl: 'mvc/views/user-groups.html',
-                controller: 'UserGroupsCtrl'
-            })
-            .when('/messenger/system-messages', {
-                templateUrl: 'mvc/views/messenger-system-messages.html',
-                controller: 'MessengerSystemMessagesCtrl'
-            })
-            .when('/messenger/admin-messages', {
-                templateUrl: 'mvc/views/messenger-admin-messages.html',
-                controller: 'MessengerAdminMessagesCtrl'
-            })
-            .when('/messenger/sys-messages', {
-                templateUrl: 'mvc/views/messenger-sys-messages.html',
-                controller: 'MessengerSysMessagesCtrl'
-            })
-            .when('/messenger/polls', {
-                templateUrl: 'mvc/views/messenger-polls.html',
-                controller: 'MessengerPollsCtrl'
-            })
-            .when('/messenger/filters', {
-                templateUrl: 'mvc/views/messenger-filters.html',
-                controller: 'MessengerFiltersCtrl'
-            })
-            .when('/messenger/triggers', {
-                templateUrl: 'mvc/views/messenger-triggers.html',
-                controller: 'MessengerTriggersCtrl'
-            })
-            .when('/messenger/jobs', {
-                templateUrl: 'mvc/views/messenger-jobs.html',
-                controller: 'MessengerJobsCtrl'
-            })
-            .when('/mail/templates', {
-                templateUrl: 'mvc/views/mail-templates.html',
-                controller: 'MailTemplatesCtrl'
-            })
-            .when('/feedbacks', {
-                templateUrl: 'mvc/views/feedbacks.html',
-                controller: 'FeedbacksCtrl'
-            })
-            .when('/report/overview', {
-                templateUrl: 'mvc/views/report-overview.html',
-                controller: 'ReportOverviewCtrl'
             })
             .otherwise({
                 redirectTo: '/login'
